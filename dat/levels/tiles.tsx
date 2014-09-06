bf2e2fc0af24fa7bcec3a14931bcfb4c90effd68
mrzzzrm <mrzesn@t-online.de>
2014-09-06 22:08:13 +0100

    add missing files

---
+++ dat/levels/tiles.tsx
@@ -0,0 +1,71 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="48" tileheight="48">
+ <image source="../tiles/tiles.png" width="288" height="576"/>
+ <tile id="58">
+  <properties>
+   <property name="rect[0]" value="0.66/0.75/0.33/0.25"/>
+   <property name="special_type" value="damaging"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="rect[0]" value="0/0.83333/0.375/0.16666"/>
+   <property name="special_type" value="damaging"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="container_anim_0" value="dat/tiles/container/coin_box/0.ani"/>
+   <property name="container_anim_1" value="dat/tiles/container/coin_box/1.ani"/>
+   <property name="container_anim_2" value="dat/tiles/container/coin_box/2.ani"/>
+   <property name="container_item" value="coin"/>
+   <property name="special_type" value="container"/>
+  </properties>
+ </tile>
+ <tile id="61">
+  <properties>
+   <property name="container_anim_0" value="dat/tiles/container/upgrader_box/0.ani"/>
+   <property name="container_anim_1" value="dat/tiles/container/upgrader_box/1.ani"/>
+   <property name="container_anim_2" value="dat/tiles/container/upgrader_box/2.ani"/>
+   <property name="container_item" value="upgrader"/>
+   <property name="special_type" value="container"/>
+  </properties>
+ </tile>
+ <tile id="62">
+  <properties>
+   <property name="container_anim_0" value="dat/tiles/container/life_box/0.ani"/>
+   <property name="container_anim_1" value="dat/tiles/container/life_box/1.ani"/>
+   <property name="container_anim_2" value="dat/tiles/container/life_box/2.ani"/>
+   <property name="container_item" value="life"/>
+   <property name="special_type" value="container"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="rect[0]" value="0.16666/0.5/0.54166/0.5"/>
+   <property name="rect[1]" value="0/0.66666/0.33333/1"/>
+   <property name="special_type" value="damaging"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="rect[0]" value="0/0/0.375/1"/>
+   <property name="rect[1]" value="0.375/0.208333/0.39583/0.7916666"/>
+   <property name="special_type" value="damaging"/>
+  </properties>
+ </tile>
+ <tile id="70">
+  <properties>
+   <property name="rect[0]" value="0.25/0/0.75/0.6"/>
+   <property name="rect[1]" value="0.71/0.6/0.29166/0.395833"/>
+   <property name="special_type" value="damaging"/>
+  </properties>
+ </tile>
+ <tile id="71">
+  <properties>
+   <property name="rect[0]" value="0/0/0.6875/0.25"/>
+   <property name="rect[1]" value="0/0.25/0.25/0.75"/>
+   <property name="special_type" value="damaging"/>
+  </properties>
+ </tile>
+</tileset>
